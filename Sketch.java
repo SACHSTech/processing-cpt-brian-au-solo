9b143cb1a59007c22de59d9216315124b3bb2125
brianau12 <brian.au25@ycdsbk12.ca>
2024-01-22 23:45:02 -0500

    boolean

--- Sketch.java
+++ Sketch.java
@@ -2,8 +2,7 @@ import processing.core.PApplet;
 import processing.core.PImage;
 
 public class Sketch extends PApplet {
-	
-	// Declare images
+
   PImage imgStartTitle;
   PImage imgBackground;
   PImage imgMisses;
@@ -19,18 +18,18 @@ public class Sketch extends PApplet {
   boolean blnGameOver;
   boolean blnKatanaSlice;
 
-  // Player lives and score
+  // Player score
   int intScore = 0;
 
   // Storing fruits into arrays and if they can be seen or not
-  float[] fltFruitY = new float[10];
-  float[] fltFruitX = new float[10];
-  float[] fltFruitY2 = new float[10];
-  float[] fltFruitX2 = new float[10];
+  float[] fltFruitY = new float[40];
+  float[] fltFruitX = new float[40];
+  float[] fltFruitY2 = new float[40];
+  float[] fltFruitX2 = new float[40];
   float[] fltBombX = new float[5];
   float[] fltBombY = new float[5];
-  boolean [] blnFruitHide = new boolean[10];
-  boolean [] blnFruitHide2 = new boolean[10];
+  boolean [] blnFruitHide = new boolean[40];
+  boolean [] blnFruitHide2 = new boolean[40];
   boolean [] blnBombHide = new boolean[5];
 
    // Fruit collision detection, fruit speed, spawn location, player lives
@@ -43,13 +42,12 @@ public class Sketch extends PApplet {
 
 
   public void settings() {
-
 	// screen size
   size(1000, 800);
 }
 
   public void setup() {
-    
+
     // Enter images
     imgStartTitle = loadImage("StartTitle.jpg");
     imgStartTitle.resize(1000, 800);
@@ -72,7 +70,7 @@ public class Sketch extends PApplet {
 
     imgFruit2 = loadImage("watermelon.png");
     imgFruit2.resize(80, 80);
-    
+
     imgBomb = loadImage("bomb.png");
     imgBomb.resize(80, 80);
 
@@ -90,86 +88,106 @@ public class Sketch extends PApplet {
       blnFruitHide[i] = false;
    }
 
-   for (int i = 0; i < fltFruitX2.length; i++) {
+    for (int i = 0; i < fltFruitX.length; i++) {
       fltFruitX2[i] = random(width);
       fltFruitY2[i] = -20 - random(800);
       blnFruitHide2[i] = false;
-  }
+    }
 
     for (int i = 0; i < fltBombX.length; i++) {
       fltBombX[i] = random(width);
       fltBombY[i] = -80 - random(800);
       blnBombHide[i] = false;
+    }
   }
-}
 
+  /**
+   * Called repeatedly, anything drawn to the screen goes here
+   */
   public void draw() {
     image(imgStartTitle, 0, 0);
     
     if (!blnGameOver && !blnStartTitle) {
       image(imgBackground, 0, 0);
-        // Draw and move fruits
-        for (int i = 0; i < fltFruitX.length; i++) {
-            if (!blnFruitHide[i]) {
-                // Draw the fruits
-                    image(imgFruit1, fltFruitX[i], fltFruitY[i], 80, 80);
-                }
 
-                // Move the fruit down
-                fltFruitY[i] += intFruitSpeed;
+    // Draw and move apples, watermelons, and bombs
+    for (int i = 0; i < fltFruitX.length; i++) {
+      if (!blnFruitHide[i]) {
+        image(imgFruit1, fltFruitX[i], fltFruitY[i], 80, 80);
+      }
+      fltFruitY[i] += intFruitSpeed;
 
-        for (int n = 5; n < fltFruitX2.length; n++) {
-            if (!blnFruitHide[n]) {
-                // Draw the fruits
-                    image(imgFruit2, fltFruitX[n], fltFruitY[n], 80, 80);
-                }
+      if (!blnFruitHide2[i]) {
+        image(imgFruit2, fltFruitX2[i], fltFruitY2[i], 80, 80);
+      }
+      fltFruitY2[i] += intFruitSpeed;
 
-                // Move the fruit down
-                fltFruitY2[n] += intFruitSpeed;
+      if (!blnBombHide[i]) {
+        image(imgBomb, fltBombX[i], fltBombY[i], 80, 80);
+      }
+      fltBombY[i] += intFruitSpeed;
 
-                // Fruit collision with katana, hide fruit, increase score
-                if (dist(fltFruitX[i], fltFruitY[i], mouseX, mouseY) < 30) {
-                    blnFruitHide[i] = true; 
-                    intScore += 10;
-                }
-                if (blnFruitHide[i] == true) {
-                    fltFruitY[i] = -50 - random(800);
-                    fltFruitX[i] = random(width);
-                }
+      // Apple collision with katana, hide fruit, increase score
+      if (dist(fltFruitX[i], fltFruitY[i], mouseX, mouseY) < 30) {
+        blnFruitHide[i] = true;
+        intScore += 10;
+      }
 
-                // Check for misses
-                if (fltFruitY[i] > height || fltFruitY2[n] > height) {
-                    fltFruitY[i] = -50 - random(800);
-                    fltFruitX[i] = random(width);
-                    fltFruitY2[n] = -50 - random(800);
-                    fltFruitX2[n] = random(width);
-                    intLives--;  // Increase the misses
-                    if (intLives == 0) {
-                        blnGameOver = true;  // Game over if 3 misses
-                    }
-                }
-            }
+      // Watermelon collision with katana, hide fruit, increase score
+      if (dist(fltFruitX2[i], fltFruitY2[i], mouseX, mouseY) < 30) {
+        blnFruitHide2[i] = true;
+        intScore += 10;
+      }
+
+      // Bomb collision with katana, display game over
+      if (dist(fltBombX[i], fltBombY[i], mouseX, mouseY) < 30) {
+        blnGameOver = true;
+      }
+      
+      
+
+      if (blnFruitHide[i] || blnFruitHide2[i] || blnBombHide[i]) {
+        fltFruitY[i] = -50 - random(800);
+        fltFruitX[i] = random(width);
+        fltFruitY2[i] = -30 - random(800);
+        fltFruitX2[i] = random(width);
+        fltBombY[i] = -80 - random(800);
+        fltBombX[i] = random(width);
+      }
+
+      // Check for misses
+      if (fltFruitY[i] > height || fltFruitY2[i] > height || fltBombY[i] > height) {
+        fltFruitY[i] = -50 - random(800);
+        fltFruitX[i] = random(width);
+        fltFruitY2[i] = -30 - random(800);
+        fltFruitX2[i] = random(width);
+        fltBombY[i] = -80 - random(800);
+        fltBombX[i] = random(width);
+        intLives--; // Increase the misses
+        if (intLives == 0) {
+          blnGameOver = true; // Game over if 3 misses
         }
+      }
     }
-    
+
     // Display lives
     for (int i = intLives; i > 0; i--) {
-      image(imgMisses, 650 + (3 - i) * 30, 20, 40, 40);
+      image(imgMisses, 700 + (3 - i) * 30, 20, 40, 40);
     }
 
     // Display game over message
     if (blnGameOver) {
       image(imgGameOver, 0, 0);
-        textSize(32);
-        fill(255, 0, 0);
-        textAlign(CENTER, CENTER);
-        textFont(createFont("arial bold", 40));
-        text("Your score: " + intScore, width / 2 - 10, height / 2 - 30);
+      textSize(32);
+      fill(255, 0, 0);
+      textAlign(CENTER, CENTER);
+      textFont(createFont("arial bold", 40));
+      text("Your score: " + intScore, width / 2 - 10, height / 2 - 30);
     }
 
     // Draw katana slice animation if active
     if (katanaSliceFrame >= 0) {
-      image(imgKatanaSlice, mouseX, mouseY, 55, 75);
+      image(imgKatanaSlice, mouseX, mouseY, 155, 175);
       katanaSliceFrame++;
 
       // Hide katana slice after a certain number of frames
@@ -177,6 +195,7 @@ public class Sketch extends PApplet {
         katanaSliceFrame = -1;
       }
     }
+  }
 }
 
 public void mouseClicked() {
@@ -189,7 +208,7 @@ public void mouseClicked() {
 
   public void playKatanaSliceAnimation() {
     imageMode(CENTER);
-    image(imgKatanaSlice, mouseX, mouseY, 155, 175);
+    image(imgKatanaSlice, mouseX, mouseY);
     katanaSliceFrame++;
 
     if(katanaSliceFrame > 10) {
@@ -207,7 +226,5 @@ public void mousePressed() {
   } 
 
   public void mouseDragged() {
-  
-  } 
-  
+  }
 }
